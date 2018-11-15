4cad9429083042012b01accc99d90d3b33b8570d
David <davidkueng@gmail.com>
2018-11-15 22:14:34 +0100

    added utf-16 range

--- CeasarsCipher.js
+++ CeasarsCipher.js
@@ -1,18 +1,26 @@
 function rot13(str) { // LBH QVQ VG!
 
     let rotStr = "";
-   
-   for (let i = 0 ; i < str.length; i++) {
-       
-       rotStr += str[i].replace(str.charAt(i), String.fromCharCode(str.charAt(i).charCodeAt()                     +13))
-   }
-   
-   console.log(rotStr)  
-       
-       
-       // 65 - 90 (A-Z)
-       
-       }
-       
-       // Change the inputs below to test
-       rot13("SERR PBQR PNZC");
+    
+    // console.log(str.charCodeAt(0))
+    
+    for (let i = 0 ; i < str.length; i++) {
+    
+        if (str.charCodeAt(i) <= 73) {
+        rotStr += str[i].replace(str.charAt(i), String.fromCharCode(str.charAt(i).charCodeAt()                     +13))
+        } else if (str.charCodeAt(i) >= 73){    
+        rotStr += str[i].replace(str.charAt(i), String.fromCharCode(str.charAt(i).charCodeAt()                     -13))
+        // } else if (str.charCodeAt(i) === str.charAt(i).match(/[^A-Z]/)) {
+        //  rotStr += str.charCodeAt(i)
+    
+        }
+    }
+    
+    console.log(rotStr) 
+        
+        // 65 - 90 (A-Z)
+        
+        }
+        
+        // Change the inputs below to test
+        rot13("SERR PBQR PNZC");
