83735270a04fa4bd69275f051ae0efc7d70a8bb0
David <davidkueng@gmail.com>
2018-11-17 15:19:58 +0100

    finished challenge (stupid solution)

--- validateTelNr.js
+++ validateTelNr.js
@@ -1,16 +1,14 @@
 function telephoneCheck(str) {
-    // Good luck!
-  // remove all special characters, whitespace and negative numbers except () 
-  // check if === 555 555 5555 or 1(must be 1) 555 555 5555
-  
-  let newStr = str.replace(/[^()\d]/g, '');
-  
-  if (newStr.length === 10 || newStr.length === 11 && newStr[0] === "1") {
-      return true
-    } else {
-      return false
+
+    let newStr = str.replace(/^-/, '(').replace(/[^()\d]/g, '');
+    
+    console.log(newStr)
+    
+    if (newStr.length === 10 || newStr.length === 11 && newStr[0] === "1" || newStr[1] === '(' && newStr[5] === ')' && newStr[0] === '1' || newStr.length === 12 && newStr[0] === '(' && newStr[4] === ')') {
+        return true
+      } else {
+        return false
+      }
     }
-  
-  }
-  
-  telephoneCheck("555-555-5555");
+    
+    telephoneCheck("555-555-5555");
