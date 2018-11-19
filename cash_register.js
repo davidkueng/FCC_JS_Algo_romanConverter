7e8e35f374c9c7477efda3966ccc98ba2717b677
David <davidkueng@gmail.com>
2018-11-19 23:21:10 +0100

    figured out the change.change loop

--- cash_register.js
+++ cash_register.js
@@ -1,41 +1,57 @@
 function checkCashRegister(price, cash, cid) {
   
-  let change = {
-    status: 'OPEN',
-    cid: [],
-    change: []
-  };
-
-  change.cid = cid.concat(change.cid)
-
-  let changeAmount = cash - price;
-
-for (let [key, value] in change.cid) { 
-  if (changeAmount > change.cid[key][1]) 
-  change.cid[key][1] -= changeAmount
-}  
-  
- console.log(change)  
-
-  // Here is your change, ma'am.
-  // return change;
-}
-
-// make change object with values status: and change:
-// push the cash into cid array
-// deduct price from cash
-// decide whether status: OPEN (if change > changeAmount) or status: INSUFFICIENT_FUNDS (if change < changeAmount) or CLOSED (if all values === 0)
-// loop over cid and push change into change: value
-
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  const currencyUnits = [["ONE HUNDRED", 100], ["TWENTY", 20], ["TEN", 10], ["FIVE", 5], ["DOLLAR",     1], ["QUARTER", 0.25], ["DIME", 0.1], ["NICKEL", 0.05], ["PENNY", 0.01]];
+ 
+   let change = {
+     status: 'OPEN',
+     cid: [],
+     change: []
+   };
+ 
+   let changeAmount = cash - price;
+   // console.log(changeAmount)
+ 
+   change.cid = cid.concat(change.cid);  
+ 
+   for (let [key, value] in currencyUnits) {
+     while (changeAmount >= currencyUnits[key][1]) {
+       change.change.push(currencyUnits[key])
+       changeAmount -= currencyUnits[key][1]
+     }       
+   }
+ 
+   console.log(change.change[0][1] + change.change[1][1])
+ 
+   for (let [key, value] in change.change) {
+     // console.log(change.change[key][1])
+     change.change[key][1] + change.change[key][1]    
+   }
+ 
+ // for (let [key, value] in change.cid) { 
+    // change.cid[key][1] = 0
+ // }
+   
+ //  console.log(change)  
+ //  console.log(newArray)  
+ 
+   // Here is your change, ma'am.
+   // return change;
+ }
+ 
+ // deduct changeAmount from the corresponding currency in cid
+ // push changeAmount with currency into new array
+ // change value of status to OPEN, CLOSED or INSUFFICIENT_FUNDS depending on status of cid
+ // return the whole change object without cid
+ 
+ // Example cash-in-drawer array:
+ // [["PENNY", 1.01],
+ // ["NICKEL", 2.05],
+ // ["DIME", 3.1],
+ // ["QUARTER", 4.25],
+ // ["ONE", 90],
+ // ["FIVE", 55],
+ // ["TEN", 20],
+ // ["TWENTY", 60],
+ // ["ONE HUNDRED", 100]]
+ 
+ checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
