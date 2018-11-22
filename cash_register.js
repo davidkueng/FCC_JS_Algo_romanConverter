47a19e0787c67c19086f375b9fecb5e6928a771c
David <davidkueng@gmail.com>
2018-11-22 21:29:16 +0100

    Final solution

--- cash_register.js
+++ cash_register.js
@@ -1,56 +1,61 @@
 function checkCashRegister(price, cash, cid) {
+
+  const currencyUnits = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]];
+   
+    let change = {
+       status: 'OPEN',
+       cid: [],
+       change: [] 
+    };
+   
+    let changeAmount = cash - price;   
   
-  // console.log(cid)
-
-const currencyUnits = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25],                             ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED",                          100]];
- 
-  let change = {
-     status: 'OPEN',
-     inDrawer: [],
-     change: [] 
-  };
- 
-  let changeArray = [];
- 
-  let changeAmount = cash - price;   
-
-  let cidTempArray = cid.map((arr) => {
-    return arr.slice();
-  });
+    let cidTempArray = cid.map((arr) => {
+      return arr.slice();
+    });
   
-   change.inDrawer = [...cid].reverse().concat(change.inDrawer);
-
-  //  console.log(change.inDrawer);
- 
-   for (let value in currencyUnits.reverse()) {
-    //  console.log(currencyUnits[value][1])
-    //  console.log(change.cid[value][1]) 
-     while (changeAmount >= currencyUnits[value][1] && change.inDrawer[value][1] !=0) {
-       changeArray.push(currencyUnits[value]); 
-       changeAmount -= currencyUnits[value][1];
-       change.inDrawer[value][1] -= currencyUnits[value][1]
-     }  
+    change.cid = [...cid].reverse().concat(change.cid);
+   
+    for (let value in currencyUnits.reverse()) {
+      while (Math.round(changeAmount * 100) /100 >= currencyUnits[value][1] 
+              && change.cid[value][1] > 0) {
+        changeAmount -= currencyUnits[value][1];
+        change.cid[value][1] -= currencyUnits[value][1]
+      }  
+    }
+  
+    for (let value in change.cid.reverse()) {
+      if (change.cid[value][1] != cidTempArray[value][1] | cidTempArray[value][1] === 0) {
+        let changeCurrency = cidTempArray[value][1] - change.cid[value][1]; 
+        change.change.push([change.cid[value][0], Math.round(changeCurrency * 100)/100])
+      }    
+    }
+  
+    change.change.sort((a, b) => b[1] - a[1])
+  
+    for (let value in change.status) {
+      if (changeAmount > 0) {
+        change.status = "INSUFFICIENT_FUNDS"
+        change.change = [];
+      } else if (change.cid[value][1] === 0) {
+        change.status = "CLOSED"
+      }
+    }  
+  
+    delete change.cid;  
+    // Here is your change, ma'am.
+    return change
    }
- 
-  console.log(cidTempArray)
-  console.log(change.inDrawer) 
-  // console.log(changeArray) 
- 
-   // Here is your change, ma'am.
-   // return change;
- }
- 
- // push the difference between cid and changed cid into change.change array
- 
- // Example cash-in-drawer array:
- // [["PENNY", 1.01],
- // ["NICKEL", 2.05],
- // ["DIME", 3.1],
- // ["QUARTER", 4.25],
- // ["ONE", 90],
- // ["FIVE", 55],
- // ["TEN", 20],
- // ["TWENTY", 60],
- // ["ONE HUNDRED", 100]]
- 
- checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+   
+   // Example cash-in-drawer array:
+   // [["PENNY", 1.01],
+   // ["NICKEL", 2.05],
+   // ["DIME", 3.1],
+   // ["QUARTER", 4.25],
+   // ["ONE", 90],
+   // ["FIVE", 55],
+   // ["TEN", 20],
+   // ["TWENTY", 60],
+   // ["ONE HUNDRED", 100]]
+   
+   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
